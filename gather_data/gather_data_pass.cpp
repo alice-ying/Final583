fb74062d21712ad522b5d956c7d4ffc719be7d3f
Nicholas Chengchen Huang <ncchuang@eecs583a.eecs.umich.edu>
2021-12-03 00:30:08 -0500

    Added brief spec in gather_data_pass

--- gather_data/gather_data_pass.cpp
+++ gather_data/gather_data_pass.cpp
@@ -21,3 +21,19 @@ char Hello::ID = 0;
 static RegisterPass<Hello> X("hello", "Hello World Pass",
                              false /* Only looks at CFG */,
                              false /* Analysis Pass */);
+
+
+/* 
+Gather Data Pass Information
+1. Given a benchmark, iterate over all branches. For each branch, record the true branch probability 
+   (probability that a branch is taken, between 0 and 1) along with that branch id
+2. Additionally for each branch, apply each heuristic on the branch and record a 1 if it is true, and a 0 if it is false
+
+Output Formats:
+true_probability.csv
+  - Rows: One for each branch
+  - Columns: branch_id, branch_probability (0 to 1)
+heuristics.csv
+  - Rows: One for each branch
+  - Columns: branch_id, loop, pointer, opcode, guard, loop_header, call, store, return
+*/
