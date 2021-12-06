ba72674fb9a0992c6818c3f3c9d8f7bb85e00c5d
Alice C Ying <acying@eecs583a.eecs.umich.edu>
2021-12-06 17:49:14 -0500

    All automotive benchmarks running

---
+++ benchmarks/automotive/susan/run.sh
@@ -0,0 +1,16 @@
+PATH2LIB=~/Final583/build/gather_data/LLVMPJT.so      # Specify your build directory in the project
+PASS=-heuristics
+
+# Canonicalize natural loops
+opt -loop-simplify ${1}.bc -o ${1}.ls.bc
+# Instrument profiler
+opt -pgo-instr-gen -instrprof ${1}.ls.bc -o ${1}.ls.prof.bc
+# # Generate binary executable with profiler embedded
+clang -fprofile-instr-generate -lm ${1}.ls.prof.bc -o ${1}_prof
+
+# # Generate profiled data
+./${1}_prof ${2}.pgm > correct_output
+llvm-profdata merge -o ${1}.profdata default.profraw
+
+# # Apply Heuristics
+opt -o ${1}.heuristics.bc -pgo-instr-use -pgo-test-profile-file=${1}.profdata -load ${PATH2LIB} ${PASS} < ${1}.ls.bc > /dev/null
