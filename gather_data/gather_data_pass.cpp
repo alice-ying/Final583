3dddccde476f223e3a788509fba33fc1531fb44d
alice-ying <ying.alice.c@gmail.com>
2021-12-12 01:18:45 -0500

    Debugged guard and opcode

--- gather_data/gather_data_pass.cpp
+++ gather_data/gather_data_pass.cpp
@@ -12,9 +12,9 @@
 // heuristics.csv
 //   - Rows: One for each branch
 //   - Columns: branch_id, loop, pointer, opcode, guard, loop_header, call, store, return
-//    -1: N/A
-//    0: taken
-//    1: not taken
+//    0.5: N/A
+//    1: taken
+//    0: not taken
 
 // Ball and Larus Heuristics:
 //   - Loop: If the branch is a loop (backedge to loop header), then predict taken.
@@ -98,15 +98,23 @@ struct CalcHeuristics : public FunctionPass {
         fprintf(true_prob_data, "%f, %f\n", taken_prob, not_taken_prob);
 
         // Heuristics
-        fprintf(heuristic_data, "%d, %d, %d, %d, %d, %d, %d, %d\n",
-          h_loop(curr_bb, taken),
-          h_pointer(curr_bb),
-          h_opcode(curr_bb),
-          h_guard(curr_bb, taken, not_taken),
-          h_loopheader(curr_bb, taken, not_taken),
-          h_call(curr_bb, taken, not_taken),
-          h_store(curr_bb, taken, not_taken),
-          h_return(taken, not_taken)
+        double loop_val = h_loop(curr_bb, taken);
+        double pointer_val = h_pointer(curr_bb);
+        double opcode_val = h_opcode(curr_bb);
+        double guard_val = h_guard(curr_bb, taken, not_taken);
+        double loopheader_val = h_loopheader(curr_bb, taken, not_taken);
+        double call_val = h_call(curr_bb, taken, not_taken);
+        double store_val = h_store(curr_bb, taken, not_taken);
+        double return_val = h_return(taken, not_taken);
+        fprintf(heuristic_data, "%.1f, %.1f, %.1f, %.1f, %.1f, %.1f, %.1f, %.1f\n",
+          loop_val,
+          pointer_val,
+          opcode_val,
+          guard_val,
+          loopheader_val,
+          call_val,
+          store_val,
+          return_val
         );
       }
     }
@@ -115,21 +123,21 @@ struct CalcHeuristics : public FunctionPass {
   }
 
   // Loop: If the branch is a loop (backedge to loop header), then predict taken.
-  int h_loop(BasicBlock *branch_bb, BasicBlock *taken_successor_bb){
+  double h_loop(BasicBlock *branch_bb, BasicBlock *taken_successor_bb){
     LoopInfo *LI = &getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
     Loop* L = LI->getLoopFor(branch_bb);
     if(L != nullptr){
       BasicBlock* header = L->getHeader();
       BasicBlock* preheader = L->getLoopPreheader();
       if(header == taken_successor_bb || preheader == taken_successor_bb){
-        return 0;
+        return 1;
       }
     }
-    return -1;
+    return 0.5;
   }
 
   // Pointer: If comparing a pointer against null or comparing two pointers with each other, then predict not taken
-  int h_pointer(BasicBlock *branch_bb){
+  double h_pointer(BasicBlock *branch_bb){
     Instruction *instr = branch_bb->getTerminator();
     BranchInst *branch_instr = dyn_cast<BranchInst>(instr);
 
@@ -141,14 +149,14 @@ struct CalcHeuristics : public FunctionPass {
       Type *opr1_ty = i->getOperand(1)->getType();
 
       if(opr0_ty->isPointerTy() && opr1_ty->isPointerTy()){
-        return 1;
+        return 0;
       }
     }
-    return -1;
+    return 0.5;
   }
  
   // Opcode: If comparing an int variable < 0, <= 0, or = any constant, then predict not taken
-  int h_opcode(BasicBlock *branch_bb){
+  double h_opcode(BasicBlock *branch_bb){
     Instruction *instr = branch_bb->getTerminator();
     BranchInst *branch_instr = dyn_cast<BranchInst>(instr);
 
@@ -159,45 +167,45 @@ struct CalcHeuristics : public FunctionPass {
       Value *opr0 = i->getOperand(0);
       Value *opr1 = i->getOperand(1);
 
-      if(opr0->getType()->isIntegerTy() && opr1->getType()->isIntegerTy()){
+      if(isa<CmpInst>(i) && opr0->getType()->isIntegerTy() && opr1->getType()->isIntegerTy()){
         CmpInst *cmpInst = dyn_cast<CmpInst>(&*i);
         CmpInst::Predicate pred = cmpInst->getPredicate();
 
         if(isa<Constant>(opr0) && !isa<Constant>(opr1)){
           int const_val = dyn_cast<llvm::ConstantInt>(opr0)->getSExtValue();
           if((const_val == 0 && (pred == CmpInst::Predicate::ICMP_SGT || pred == CmpInst::Predicate::ICMP_SGE))
-            || (const_val != 0 && pred == CmpInst::Predicate::ICMP_EQ)){
-            return 1;
+            || (pred == CmpInst::Predicate::ICMP_EQ)){
+            return 0;
           }
         }
         else if(!isa<Constant>(opr0) && isa<Constant>(opr1)){
           int const_val = dyn_cast<llvm::ConstantInt>(opr1)->getSExtValue();
           if((const_val == 0 && (pred == CmpInst::Predicate::ICMP_SLT || pred == CmpInst::Predicate::ICMP_SLE))
-            || (const_val != 0 && pred == CmpInst::Predicate::ICMP_EQ)){
-            return 1;
+            || (pred == CmpInst::Predicate::ICMP_EQ)){
+            return 0;
           }
         }
       }
     }
-    return -1;
+    return 0.5;
   }
 
   Value* check_load(Instruction *inst){
-    if(inst->getOpcode() == llvm::Instruction::Load){
+    if(inst != nullptr && inst->getOpcode() == llvm::Instruction::Load){
       return inst->getOperand(0);
     }
     return nullptr;
   }
 
   Value* check_store(Instruction *inst){
-    if(inst->getOpcode() == llvm::Instruction::Store){
+    if(inst != nullptr && inst->getOpcode() == llvm::Instruction::Store){
       return inst->getOperand(1);
     }
     return nullptr;
   }
 
   // Guard: If operand is a register that is used before define in the successor, and the successor does not post-dominate the current block, predict branch taken to the successor
-  int h_guard(BasicBlock *branch_bb, BasicBlock *taken_successor_bb, BasicBlock *not_taken_successor_bb){
+  double h_guard(BasicBlock *branch_bb, BasicBlock *taken_successor_bb, BasicBlock *not_taken_successor_bb){
     auto PDT = &getAnalysis<PostDominatorTreeWrapperPass>().getPostDomTree();
     Instruction *instr = branch_bb->getTerminator();
     BranchInst *branch_instr = dyn_cast<BranchInst>(instr);
@@ -208,10 +216,7 @@ struct CalcHeuristics : public FunctionPass {
       Instruction *instopr1 = dyn_cast<Instruction>(i->getOperand(0));
       Instruction *instopr2 = dyn_cast<Instruction>(i->getOperand(1));
       Value* opr1 = check_load(instopr1);
-      Value* opr2 = nullptr;
-      if(instopr2 != nullptr){
-        opr2 = check_load(instopr2);
-      }
+      Value* opr2 = check_load(instopr2);
 
       if(!PDT->dominates(taken_successor_bb, branch_bb)){
         bool opr1_def_before_use = false;
@@ -219,10 +224,10 @@ struct CalcHeuristics : public FunctionPass {
         // Loop until both operands are defined before used, or one is used before defined. If the latter is the case, predict taken!
         for(Instruction &i : *taken_successor_bb) {
           // Only check opr1 if it is not already defined before used
-          if(!opr1_def_before_use){
+          if(!opr1_def_before_use && opr1 != nullptr){
             // Check if opr1 is used as an operand
             if(check_load(&i) == opr1){
-              return 0;
+              return 1;
             }
             // Check if opr1 is the destination
             if(check_store(&i) == opr1){
@@ -232,7 +237,7 @@ struct CalcHeuristics : public FunctionPass {
           if(!opr2_def_before_use && opr2 != nullptr){
             // Check if opr2 is used as an operand
             if(check_load(&i) == opr2){
-              return 0;
+              return 1;
             }
             // Check if opr2 is the destination
             if(check_store(&i) == opr2){
@@ -255,7 +260,7 @@ struct CalcHeuristics : public FunctionPass {
           if(!opr1_def_before_use){
             // Check if opr1 is used as an operand
             if(check_load(&i) == opr1){
-              return 1;
+              return 0;
             }
             // Check if opr1 is the destination
             if(check_store(&i) == opr1){
@@ -265,7 +270,7 @@ struct CalcHeuristics : public FunctionPass {
           if(!opr2_def_before_use && opr2 != nullptr){
             // Check if opr1 is used as an operand
             if(check_load(&i) == opr2){
-              return 1;
+              return 0;
             }
             // Check if opr2 is the destination
             if(check_store(&i) == opr2){
@@ -278,35 +283,32 @@ struct CalcHeuristics : public FunctionPass {
           }
         }
       }
-      return -1;
-    }else{
-      return -1;
+      return 0.5;
     }
+    return 0.5;
   }
 
   // Loop Header: If successor is a loop header or loop pre-header and does not post dominate, then the branch will be taken
-  int h_loopheader(BasicBlock *branch_bb, BasicBlock *taken_successor_bb, BasicBlock *not_taken_successor_bb){
+  double h_loopheader(BasicBlock *branch_bb, BasicBlock *taken_successor_bb, BasicBlock *not_taken_successor_bb){
     auto PDT = &getAnalysis<PostDominatorTreeWrapperPass>().getPostDomTree();
     LoopInfo *LI = &getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
     if(!PDT->dominates(taken_successor_bb, branch_bb) && (LI->isLoopHeader(taken_successor_bb) /* or loop pre-header */)){
-      errs() << 0 << "\n";
-      return 0;
+      return 1;
     }
 
     if(not_taken_successor_bb != nullptr && !PDT->dominates(not_taken_successor_bb, branch_bb) && (LI->isLoopHeader(not_taken_successor_bb) /* or loop pre-header */)){
-      errs() << 1 << "\n";
-      return 1;
+      return 0;
     }
-    return -1;
+    return 0.5;
   }
 
   // Call: If successor contains a function call and does not post dominate, predict branch not taken
-  int h_call(BasicBlock *branch_bb, BasicBlock *taken_successor_bb, BasicBlock *not_taken_successor_bb){
+  double h_call(BasicBlock *branch_bb, BasicBlock *taken_successor_bb, BasicBlock *not_taken_successor_bb){
     auto PDT = &getAnalysis<PostDominatorTreeWrapperPass>().getPostDomTree();
     if(!PDT->dominates(taken_successor_bb, branch_bb)){
       for(Instruction &i : *taken_successor_bb) {
         if(isa<CallInst>(&i)){
-          return 1;
+          return 0;
         }
       }
     }
@@ -315,21 +317,21 @@ struct CalcHeuristics : public FunctionPass {
       for(Instruction &i : *not_taken_successor_bb) {
         int opc = i.getOpcode();
         if(isa<CallInst>(&i)){
-          return 0;
+          return 1;
         }
       }
     }
-    return -1;
+    return 0.5;
   }
 
   // Store: If succeessor contains a store and does not post dominate, predict branch not taken
-  int h_store(BasicBlock *branch_bb, BasicBlock *taken_successor_bb, BasicBlock *not_taken_successor_bb){
+  double h_store(BasicBlock *branch_bb, BasicBlock *taken_successor_bb, BasicBlock *not_taken_successor_bb){
     auto PDT = &getAnalysis<PostDominatorTreeWrapperPass>().getPostDomTree();
     if(!PDT->dominates(taken_successor_bb, branch_bb)){
       for(Instruction &i : *taken_successor_bb) {
         int opc = i.getOpcode();
         if(opc == llvm::Instruction::Store){
-          return 1;
+          return 0;
         }
       }
     }
@@ -338,19 +340,19 @@ struct CalcHeuristics : public FunctionPass {
       for(Instruction &i : *not_taken_successor_bb) {
         int opc = i.getOpcode();
         if(opc == llvm::Instruction::Store){
-          return 0;
+          return 1;
         }
       }
     }
-    return -1;
+    return 0.5;
   }
 
   // Return: If successor contains a return, predict branch not taken
-  int h_return(BasicBlock *taken_successor_bb, BasicBlock *not_taken_successor_bb){
+  double h_return(BasicBlock *taken_successor_bb, BasicBlock *not_taken_successor_bb){
     for(Instruction &i : *taken_successor_bb) {
         int opc = i.getOpcode();
         if(opc == llvm::Instruction::Ret){
-            return 1;
+            return 0;
         }
     }
 
@@ -358,12 +360,11 @@ struct CalcHeuristics : public FunctionPass {
       for(Instruction &i : *not_taken_successor_bb) {
         int opc = i.getOpcode();
         if(opc == llvm::Instruction::Ret){
-          return 0;
+          return 1;
         }
       }
     }
-
-    return -1;
+    return 0.5;
   }
 };
 };
